8be48b3da29871f988c40909f7e2cf69ac41ba9b
sierikov <artem.sierikov@gmail.com>
2020-09-15 01:30:36 +0300

    Add Task 1

---
+++ tasks/1-type-and-value/solution.scala
@@ -0,0 +1,7 @@
+object Task extends App {
+  val res0: Int = 1 + 2 // res0 = 3
+  val res1: Int = "3".toInt // res1 = 3
+
+  /* The type of "foo".toInt expression is Int.
+    But the evaluation of the expression fails. */
+}
---
+++ tasks/1-type-and-value/task.md
@@ -0,0 +1,9 @@
+# Type and Value
+
+**Reference**: 2.1.4
+
+**Description**: Using the Scala console or worksheet, determine the *type* and *value* of the followind expressions:
+
+- `1 + 2`
+- `"3".toInt`
+- `"foo".toInt`
