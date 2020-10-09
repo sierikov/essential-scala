33a404ac2bc3a13065df096744b8b1fcc9178b21
sierikov <artem.sierikov@gmail.com>
2020-10-09 15:57:56 +0300

    Add A Calculator - Stage 2

--- projects/1-a-calculator/Expression.scala
+++ projects/1-a-calculator/Expression.scala
@@ -1,4 +1,13 @@
-sealed trait Expression
-final case class Addition(left: Expression, right: Expression) extends Expression
-final case class Subtraction(left: Expression, right: Expression) extends Expression
+sealed trait Expression {
+  def eval: Double =
+    this match {
+      case Addition(left, right)    => left.eval + right.eval
+      case Subtraction(left, right) => left.eval + right.eval
+      case Number(value)            => value
+    }
+}
+final case class Addition(left: Expression, right: Expression)
+    extends Expression
+final case class Subtraction(left: Expression, right: Expression)
+    extends Expression
 final case class Number(value: Double) extends Expression
