33188c81eeaf488e1d3265f66c3452fdd71ead44
sierikov <artem.sierikov@gmail.com>
2020-11-13 17:34:06 +0100

    Add Calculator Again - Optimize

--- projects/3-calculator-again/src/calculator/Expression.scala
+++ projects/3-calculator-again/src/calculator/Expression.scala
@@ -1,27 +1,20 @@
 package calculator
 
 trait Expression {
+  def lift(left: Expression, right: Expression, f: (Double, Double) => Sum[String, Double]): Sum[String, Double] =
+    left.eval.flatMap { l =>
+      right.eval.flatMap { r =>
+        f(l, r)
+      }
+    }
+
   def eval: Sum[String, Double] =
     this match {
-      case Addition(left, right) =>
-        left.eval.flatMap { l =>
-          right.eval.flatMap { r =>
-            Success(l + r)
-          }
-        }
-      case Subtraction(left, right) =>
-        left.eval.flatMap { l =>
-          right.eval.flatMap { r =>
-            Success(l - r)
-          }
-        }
-      case Division(left, right) =>
-        left.eval.flatMap { l =>
-          right.eval.flatMap { r =>
-            if (r == 0) Failure("Division by zero")
-            else Success(l / r)
-          }
-        }
+      case Addition(left, right) => lift(left, right, (l, r) => Success(l + r))
+      case Subtraction(left, right) => lift(left, right, (l, r) => Success(l - r))
+      case Division(left, right) => lift(left, right, (l, r) =>
+        if (r == 0) Failure("Division by zero")
+        else Success(l / r))
       case SquareRoot(value) =>
         value.eval.flatMap { v =>
           if (v > 0) Success(Math.sqrt(v))
