c2977ba37d461e69c8cdf6c41819cd6db061abc8
sierikov <artem.sierikov@gmail.com>
2020-11-26 00:59:52 +0100

    Add Do it yourself - unique

--- tasks/29-do-it-yourself/solution.scala
+++ tasks/29-do-it-yourself/solution.scala
@@ -1,10 +1,17 @@
 case object Utils {
   def smallest(seq: Seq[Int]): Int =
     seq.foldLeft(Int.MaxValue)((a, b) => if (a < b) a else b)
+
+  def unique(seq: Seq[Int]): Seq[Int] =
+    seq.foldLeft(Seq.empty[Int])((seq, a) =>
+      if (!seq.contains(a)) seq :+ a
+      else seq
+    )
 }
 
 object solution extends App {
-  val seq = Seq(52, 85, 1, 8, 3, 2, 7)
+  val seq = Seq(52, 85, 1, 2, 1, 1, 3, 8, 3, 2, 7)
 
   Utils.smallest(seq)
+  Utils.unique(seq)
 }
