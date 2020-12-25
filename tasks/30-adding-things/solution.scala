c5a11175e9ec3e6b65965d6dca1f333b6b03fcdb
sierikov <artem.sierikov@gmail.com>
2020-12-26 00:54:47 +0100

    Add Adding things - divide

--- tasks/30-adding-things/solution.scala
+++ tasks/30-adding-things/solution.scala
@@ -7,9 +7,23 @@ object Utils {
 
   def addOptionsMap(one: Option[Int], two: Option[Int]): Option[Int] =
     one.flatMap(x => two.map(y => x + y))
+
+  def divide(numerator: Int, denominator: Int) =
+    if(denominator == 0) None
+    else Some(numerator / denominator)
+
+  def divideOptions(numerator: Option[Int], denominator: Option[Int]) =
+    for {
+      n <- numerator
+      d <- denominator
+      r <- divide(n, d)
+    } yield r
 }
 
 object solution extends App {
   Utils.addOptions(Some(3), Some(2))
   Utils.addOptionsMap(Some(3), Some(2))
+
+  Utils.divide(10, 5)
+  Utils.divideOptions(Some(10), Some(5))
 }
--- tasks/30-adding-things/task.md
+++ tasks/30-adding-things/task.md
@@ -9,3 +9,13 @@ to structure your code.
 
 Write a second version of your code using `map` and `flatMap`
 instead of a for comprehension.
+
+*Stage 2*: A(nother) Short Division Exercise
+
+Write a method `divide` that accepts two `Int` parameters and
+divides one by the other. Use `Option` to avoid exceptions
+when the denominator is 0.
+
+Using your `divide` method and a for comprehension, write
+a method called `divideOptions` that accepts two parameters
+of type `Option[Int]` and divides one by the other:
